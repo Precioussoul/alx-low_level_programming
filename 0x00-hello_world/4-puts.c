155a7b560c76bba3a84113b84cae3cd270bea121
Sofiyullah Abdullah <habsoff@gmail.com>
2022-03-10 06:46:52 -0800

    fixed: betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  *main - main block
  *Return: 0
  */
-
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle \n");
+  puts("\"Programming is like building a multilingual puzzle \n");
   return 0;
 }
