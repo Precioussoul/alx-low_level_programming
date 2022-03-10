13e7f668cf204f221001b8103a8dea7b84bc4af6
Sofiyullah Abdullah <habsoff@gmail.com>
2022-03-10 08:45:24 -0800

    feat: added 101-quote.c after ,many fixes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - main block
+ *Return: 1
+ */
+int main(void)
+{
+char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, word, sizeof(word));
+return (1);
+}
