a2ead39882077cd56940bd8dc3d24552ac1dbb37
Sofiyullah Abdullah <habsoff@gmail.com>
2022-03-11 13:42:48 -0800

    5-print-c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+i = '0';
+for (; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
