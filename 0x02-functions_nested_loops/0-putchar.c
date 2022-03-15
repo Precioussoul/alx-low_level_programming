f98ceb30eca8807e9bde93db7a9b5937726a5ef0
Sofiyullah Abdullah <habsoff@gmail.com>
2022-03-15 12:54:04 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,20 +4,16 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
-  char word[9] = "_putchar";
-
-  int i = 0;
-
-
-  while (i < 8)
-    {
-      _putchar(word[i]);
-      i++;
-    }
-  _putchar('\n');
-  return (0);
+char ch[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(ch[i]);
+i++;
 }
- 
+_putchar('\n');
+return (0);
+}
+
